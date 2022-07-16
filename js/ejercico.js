9b25473764934b0ecf37080a20313adaf7cba1ad
Yessica Juarez <yessica48206@gmail.com>
2022-07-16 16:43:25 -0300

    hice los 3 primeros ejercicio y el cuarto tengo que modificar

--- js/ejercico.js
+++ js/ejercico.js
@@ -1,3 +1,15 @@
 //1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
 
-alert("hoy es un lindo dia para sonreir")
+alert("hoy es un lindo dia para sonreir");
+
+//2.Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+document.write("<h2> Hola mundo </h2> ")
+
+// 3.Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+document.write("<p> 3+5 = "+ (3+5) + "</p>")
+
+//4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+Nombre = prompt("Ingrese el nombre");
+console.log(Nombre);
+ducument.write("<p> Hola " + Nombre + "</p>");
