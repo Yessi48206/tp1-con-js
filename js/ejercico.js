e281f19eb520587815fa643cfd62f05c6a310f8d
Yessica Juarez <yessica48206@gmail.com>
2022-07-18 10:08:59 -0300

    los 3 primeros puntos completo

--- js/ejercico.js
+++ js/ejercico.js
@@ -10,6 +10,11 @@ document.write("<h2> Hola mundo </h2> ")
 document.write("<p> 3+5 = "+ (3+5) + "</p>")
 
 //4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
-Nombre = prompt("Ingrese el nombre");
-console.log(Nombre);
-ducument.write("<p> Hola " + Nombre + "</p>");
+
+
+//5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+//let numeroUno= parseInt(prompt("ingrese el primer numero"));
+//let numeroDos= parseInt(prompt("ingrese elsegundo numero"));
+//console.log (parseInt(numeroUno));
+//console.log (parseInt(numeroDos));
+//document.write("<p>La suma de los dos numero ingresado es :"+(numeroUno+numeroDos)+"</p>");
