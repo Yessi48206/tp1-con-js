567c2d27278396910e4e29ded421969392ab9ccb
Yessica Juarez <yessica48206@gmail.com>
2022-07-18 10:25:48 -0300

    hice el punto 4 y 5

---
+++ js/ejercicio-4-5.js
@@ -0,0 +1,12 @@
+//Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+let Nombre= prompt("Ingrese el nombre");
+console.log(Nombre);
+document.write("<p> Hola "+ Nombre+"</p>");
+
+//5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+let Numero1= parseInt(prompt("Ingrese un numero"));
+let Numero2= parseInt(prompt("Ingrese un numero"));
+console.log(Numero1);
+console.log(Numero2);
+document.write("<p> La suma de los numero ingresado es :"+(Numero1+Numero2)+"</p>");
